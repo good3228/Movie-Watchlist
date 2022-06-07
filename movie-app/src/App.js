c8a621cbe9bc1ff524b5fe4c7888becf0537a960
Chun Lee <jim19980815@gmail.com>
2022-06-07 15:19:44 +0800

    react-router v1

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,16 +1,30 @@
-import './App.css';
-import React from 'react';
-import Header from './Components/Header/Header.js';
-import SimpleBottomNavigation from './Components/MainNav';
+import "./App.css";
+import Header from "./Components/Header/Header.js";
+import SimpleBottomNavigation from "./Components/MainNav";
+import { Container } from "@mui/system";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Trending from "./Pages/Trending/Trending";
+import Movies from "./Pages/Movies/Movies";
+import Series from "./Pages/Series/Series";
+import Search from "./Pages/Search/Search";
 
 function App() {
-
   return (
     <>
-      <Header />
-      <div className="App">
-        <SimpleBottomNavigation></SimpleBottomNavigation>
-      </div>
+      <BrowserRouter>
+        <Header />
+        <div className="App">
+          <Container>
+            <Switch>
+              <Route path="/" component={Trending} exact />
+              <Route path="/movies" component={Movies} />
+              <Route path="/series" component={Series} />
+              <Route path="/search" component={Search} />
+            </Switch>
+          </Container>
+          <SimpleBottomNavigation></SimpleBottomNavigation>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
