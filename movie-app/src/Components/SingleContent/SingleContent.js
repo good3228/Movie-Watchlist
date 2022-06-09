169e892a8c75a3fa34805350c61366c1f0983180
Chun Lee <jim19980815@gmail.com>
2022-06-09 22:15:52 +0800

    SingleContent -v2

--- movie-app/src/Components/SingleContent/SingleContent.js
+++ movie-app/src/Components/SingleContent/SingleContent.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import "./SingleContent.css";
+import { img_300, unavailable} from "../../config/config";
 const SingleContent = ({
   id,
   poster,
@@ -8,7 +9,16 @@ const SingleContent = ({
   media_type,
   vote_average,
 }) => {
-    return <div>{title}</div>;
+    return (
+      <div className="media">
+            <img className ="poster" src={poster ? `${img_300}/${poster}` : unavailable}></img>
+            <b className="title">{title}</b>
+            <span className="sunTitle">
+                {media_type === "tv" ? "TV Series" : "Movies"}
+                <span className="date">{date}</span>
+            </span>
+      </div>
+    );
 };
 
 export default SingleContent;
